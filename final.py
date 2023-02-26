aa408522d7f827c2a2fcc10c90b53041c9a2ca57
j-lagr <joshualagrata@gmail.com>
2023-02-26 22:43:27 +0800

    total bill function

--- final.py
+++ final.py
@@ -12,3 +12,34 @@ def Quantity():
 def Menu_Item():
     m = menu_item.get()
     return m
+
+def Price():
+    p = price.get()
+    return p
+
+def list_menu():
+    Quantity()
+    Menu_Item()
+    Price()
+    for n in range(1):
+        n=0
+        listbox_quantity.insert(n,Quantity())
+        listbox_menu_item.insert(n,Menu_Item())
+        listbox_menu_price.insert(n,Price())
+
+def bill_total():
+    quan = []
+    total = []
+    for item in listbox_quantity.get(0, "end"):
+        quan.append(item)
+    print(quan)
+    for item in listbox_menu_price.get(0, "end"):
+        total.append(item)
+    print(total)
+    products=[]
+    for quan,total in zip(quan,total):
+        products.append(quan*total)
+    total_bill= sum(products)
+    print(total_bill)
+    total_cost.set("${:,.2f}".format(float(total_bill)))
+    return total_cost
