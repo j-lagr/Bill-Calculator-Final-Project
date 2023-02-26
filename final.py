adefcb258f1c4d9666f753f638c9dfc3529c7c06
j-lagr <joshualagrata@gmail.com>
2023-02-26 22:36:29 +0800

    adding the functions

--- final.py
+++ final.py
@@ -5,3 +5,10 @@ root = Tk()
 root.geometry ("450x500")
 root.title ("Restaurant Bill")
 
+def Quantity():
+    q = quantity.get()
+    return q
+
+def Menu_Item():
+    m = menu_item.get()
+    return m
