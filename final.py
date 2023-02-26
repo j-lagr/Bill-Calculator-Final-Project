422df5a064cd68005fb9eb58bb030f930e22b470
j-lagr <joshualagrata@gmail.com>
2023-02-26 22:53:53 +0800

    creating the boxes and adding fnuctionality

--- final.py
+++ final.py
@@ -48,3 +48,32 @@ quantity = IntVar()
 menu_item= StringVar()
 price= IntVar()
 total_cost = StringVar()
+
+label_quantity= Label(root, text= "Quantity:").place(x=25,y=25)
+label_menu_item= Label(root, text= "Menu Item: ").place(x=25,y=75)
+label_price= Label(root, text= "Price:").place(x=25,y=125)
+
+entry_quantity= Entry(root, textvariable= quantity).place(x=150,y=25)
+entry_menu_item= Entry(root, textvariable= menu_item).place(x=150,y=75)
+entry_price= Entry(root, textvariable= price).place(x=150,y=125)
+
+add_item_button= Button(root,text="Add Item",width=15,command=list_menu).place(x=300,y=25)
+total_bill_button= Button(root,text="Total Bill",width=15,command=bill_total).place(x=300,y=75)
+
+label_list_quantity= Label(root, text= "Quantity:").place(x=25,y=180)
+listbox_quantity= Listbox(root,height=10,width=15)
+listbox_quantity.place(x=25,y=200)
+
+label_list_menu_item= Label(root, text= "Menu Item:").place(x=150,y=180)
+listbox_menu_item= Listbox(root,height=10,width=20)
+listbox_menu_item.place(x=150,y=200)
+
+label_list_price= Label(root, text= "Price:").place(x=300,y=180)
+listbox_menu_price= Listbox(root,height=10,width=18)
+listbox_menu_price.place(x=300,y=200)
+
+label_total_cost= Label(root, text= "Total cost:").place(x=200,y=380)
+labeloutputnetvalue = Label(root, width=15, borderwidth=2, relief="sunken", textvariable=total_cost, anchor="center")
+labeloutputnetvalue.place(x=300, y=380)
+
+root.mainloop()
